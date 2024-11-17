60af4b01b07149a96b87b88c6e18c0c7a7293c04
kartapov_ba <kartapov_ba@magnit.ru>
2024-11-17 23:37:40 +0300

    не тестировано, логика в слепую

--- lib/data/storage/storage.dart
+++ lib/data/storage/storage.dart
@@ -3,8 +3,9 @@ import 'package:happy_pet/data/dto/category/category.dart';
 import 'package:happy_pet/data/dto/pet/pet.dart';
 import 'package:happy_pet/data/dto/tag/tag.dart';
 import 'package:happy_pet/data/dto/user/user.dart';
+import 'package:happy_pet/data/storage/pet/pet_storage.dart';
+import 'package:happy_pet/data/storage/user/user_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:path_provider/path_provider.dart';
 
 class Storage {
   factory Storage() {
@@ -16,15 +17,21 @@ class Storage {
   static final Storage _singleton = Storage._();
 
   Future<void> init() async {
-    WidgetsFlutterBinding.ensureInitialized();
-
-    final directory = await getApplicationDocumentsDirectory();
-    debugPrint(directory.path);
-
     await Hive.initFlutter();
+
+    _registerAdapters();
+    await _initBoxes();
+  }
+
+  void _registerAdapters() {
     Hive.registerAdapter(UserDTOAdapter());
     Hive.registerAdapter(PetDTOAdapter());
     Hive.registerAdapter(CategoryDTOAdapter());
     Hive.registerAdapter(TagDTOAdapter());
+  }
+
+  Future<void> _initBoxes() async {
+    await UserStorage().init();
+    await PetStorage().init();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:happy_pet/data/storage/pet/pet_storage.dart';
 import 'package:happy_pet/data/storage/storage.dart';
-import 'package:happy_pet/data/storage/user/user_storage.dart';
 import 'package:happy_pet/happy_pet.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Storage().init();
-  await UserStorage().init();
-  await PetStorage().init();
 
   runApp(const HappyPet());
 }
